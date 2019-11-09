7379fe18127cece88df19b09a0bc46783c0f166e
Kristeljava <37842049+Kristeljava@users.noreply.github.com>
2019-11-09 22:36:58 +0200

    profile ja courses vahetuvad + header ja footer

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="header">
+        <strong>Welcome to your dashboard!</strong>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style scoped>
+    .header {
+        padding: 20px;
+        background-color: #2196F3;
+        color: #ffffff;
+        text-align: center;
+        margin-bottom: 10px;
+        height: 60px;
+    }
+</style>
